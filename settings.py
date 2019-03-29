be0e40b82b44267defef4b694cd73c4d10b24697
ColorDoge <48126397+ColorDoge@users.noreply.github.com>
2019-03-29 09:08:46 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,24 @@
+# coding=gbk
+import os
+
+# Redis数据库地址
+REDIS_HOST = '127.0.0.1'
+
+# Redis端口
+REDIS_PORT = 6379
+
+# Redis密码，如无填None
+REDIS_PASSWORD = None
+
+REDIS_KEY = 'proxies'
+
+#
+
+# 下载地址
+DownLoad_Path = "F:\\Treasure\\1024\\DOWN"
+
+# 整合地址
+Final_Path = "F:\\Treasure\\1024\\FINAL"
+
+#记录地址
+BackUp_Path = "F:\\Treasure\\1024\\back_up"
